bca10b2b67eec5de6245614db03969b86816f4a8
a1l1ke <onetenthplease@gmail.com>
2026-06-16 10:37:24 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 주석 시스템은 JS와 동일 (cmd/ctrl + /)
+        // 자바는 ''나 ``을 문자열용으로 지원하지 X
+        // ''는 문자 - char
+        System.out.println("Hello World!");
+        String a = "1";
+        System.out.println(a);
+    }
+}
