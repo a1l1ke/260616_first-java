72687f82b2973325be5f3d1871e5e8393356e6ec
a1l1ke <onetenthplease@gmail.com>
2026-06-16 11:17:27 +0900

    update: Main.java 출력문 및 블록 추가

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,20 @@ public class Main {
         // 주석 시스템은 JS와 동일 (cmd/ctrl + /)
         // 자바는 ''나 ``을 문자열용으로 지원하지 X
         // ''는 문자 - char
+        {
+            {
+                {
+                    {
+                        {
+                            {
+                            }
+                        }
+                    }
+                }
+            }
+        }
         System.out.println("Hello World!");
+        System.out.println("Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World!");
         String a = "1";
         System.out.println(a);
     }
